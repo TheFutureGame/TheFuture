c5ba24508dd8ce7962693c8b6b49ceb908603f78
DevAlone2 <dev2@d3d.info>
2018-12-28 21:05:49 +0300

    simulation speed widget

---
+++ Assets/Scripts/SimulationSpeedSlider.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SimulationSpeedSlider : MonoBehaviour
+{
+    public string[] valueToTextMap;
+    public Text simulationSpeedText;
+
+    private Slider slider;
+
+    void Start()
+    {
+        var slider = GetComponent<Slider>();
+        SetSimulationSpeedText(slider.value);
+        slider.onValueChanged.AddListener(SetSimulationSpeedText);
+    }
+
+    void SetSimulationSpeedText(float value)
+    {
+        int intValue = (int)value;
+        if (valueToTextMap.Length > 0 && intValue >= 0 && intValue < valueToTextMap.Length)
+        {
+            simulationSpeedText.text = valueToTextMap[intValue];
+        }
+    }
+}
