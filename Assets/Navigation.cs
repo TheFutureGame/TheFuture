f1492ee25450a6593a37acaea308a0e816aa0442
DevAlone2 <dev2@d3d.info>
2018-12-28 19:04:39 +0300

    Add another scene and UI to switch between them

---
+++ Assets/Navigation.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Navigation : MonoBehaviour
+{
+    public Button openGlobeButton;
+    public Button openSampleSceneButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        openGlobeButton.onClick.AddListener(() =>
+        {
+            SceneManager.LoadScene("Globe", LoadSceneMode.Single);
+        });
+        openSampleSceneButton.onClick.AddListener(() =>
+        {
+            SceneManager.LoadScene("SampleScene", LoadSceneMode.Single);
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
