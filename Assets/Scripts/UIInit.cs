e82bb7b69c99505d9c8cacb99d9c65e8844a4b25
DevAlone2 <dev2@d3d.info>
2018-12-29 14:32:56 +0300

    singleton UI

---
+++ Assets/Scripts/UIInit.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+class UIManager : Singleton<UIManager>
+{
+    public GameObject uiInstance;
+}
+
+public class UIInit : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+    private void Awake()
+    {
+        var uiManager = UIManager.Instance;
+        if (uiManager.uiInstance != null && uiManager.uiInstance != gameObject)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(uiManager.uiInstance = gameObject);
+        }
+    }
+}
