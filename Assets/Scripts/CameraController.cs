b760cee4578dd087e12cf2e58b359eb0335838ea
DevAlone2 <dev2@d3d.info>
2018-12-29 15:10:19 +0300

    rotating camera

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float rotationSpeed;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        float horizontalRotation = -Input.GetAxis("Horizontal") * rotationSpeed;
+        float verticalRotation = Input.GetAxis("Vertical") * rotationSpeed;
+
+        transform.Rotate(verticalRotation, horizontalRotation, 0);
+    }
+}
