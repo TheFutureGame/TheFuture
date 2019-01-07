4ef2a7fc95aef25f95ea5b9e0a32c64484c65229
DevAlone2 <dev2@d3d.info>
2019-01-07 17:24:06 +0300

    technology class

---
+++ Assets/Scripts/Technology.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum TechnologyType
+{
+    Turrets,
+}
+
+public class Technology
+{
+    public float researchCost;
+    public TechnologyType type;
+    public bool isResearching = false;
+    public bool isResearched = false;
+}
